c0193670f59d8cb987d9b4f0ce5d444e91a018b0
Denis Krestov <rupo@list.ru>
2024-03-01 17:44:26 +0300

    Create main layout

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,37 @@
 import React from 'react';
 import './App.css';
+import logo from './assets/logo.svg'
 
 function App() {
     return (
-        <div className="App">
-            Let's go, Denis, let's go....
+        <div className={'app-wrapper'}>
+            <header>
+                <h1>Social Network by Crusader & Dimi4: OPENING SOON!!!!</h1>
+            </header>
+            <aside>
+                <img src={logo} alt="crusader's social network"/>
+                <nav>
+                    <ul>
+                        <li>Profile</li>
+                        <li>Messages</li>
+                        <li>News</li>
+                        <li>Music</li>
+                    </ul>
+                    <ul>
+                        <li>Settings</li>
+                    </ul>
+                </nav>
+
+            </aside>
+            <main>
+                <div className="main-wrapper">
+                    Main content
+                </div>
+            </main>
         </div>
     );
 }
 
 export default App;
+
+
